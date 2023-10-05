c8ded9c84c4c45e9fb0e2c7edbd2e3698ddcbd7b
Gil Fernandes <gil.fernandes@onepointltd.com>
2023-10-05 19:39:45 +0100

    Added missing session number file

---
+++ data_questionnaire_agent/ui/model/session_number_container.py
@@ -0,0 +1,15 @@
+
+
+class SessionNumberContainer:
+    def __init__(self) -> None:
+        self.counter = 0
+
+    def current(self):
+        return self.counter
+
+    def increment_and_get(self):
+        self.counter += 1
+        return self.counter
+
+    def __repr__(self) -> str:
+        return f"{self.counter}"
