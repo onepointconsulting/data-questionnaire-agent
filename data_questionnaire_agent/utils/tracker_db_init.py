08572c831f46b4c12fc6fd173739e9cf631357c0
Gil Fernandes <gil.fernandes@onepointltd.com>
2023-12-03 19:09:27 +0000

    Added small utility to setup the SQL lite database

--- data_questionnaire_agent/utils/tracker_db_init.py
+++ data_questionnaire_agent/utils/tracker_db_init.py
@@ -1,4 +1,9 @@
-from chainlit.onepoint.tracker_db import create_table
+from pathlib import Path
+from chainlit.onepoint.tracker_db import ONEPOINT_SQL_LITE_DB, create_table
 
 if __name__ == "__main__":
+    db_path = Path(ONEPOINT_SQL_LITE_DB)
+    if not db_path.exists():
+        db_path.mkdir(parents=True)
+    assert db_path.exists()
     create_table()
