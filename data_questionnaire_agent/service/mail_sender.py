ef593023c5362ecd4b36ed08e1c3bfad6ea012b7
Gil Fernandes <gil.fernandes@onepointltd.com>
2024-08-08 09:57:12 +0100

    Added separate banner for email

--- data_questionnaire_agent/service/mail_sender.py
+++ data_questionnaire_agent/service/mail_sender.py
@@ -72,7 +72,7 @@ def create_mail_body(
     mail_template_text = mail_template.read_text(encoding="utf-8")
     content = f"""
 
-    <img src="{t("banner_link", locale=language)}" style="width: 100%;" />
+    <img src="{t("banner_link_email", locale=language)}" style="width: 100%;" />
 
     <p>{t("A big thank you for completing a session with", name=cfg.product_title, locale=language)}</p>
     <h2>{t("Transcript", locale=language)}</h2>
