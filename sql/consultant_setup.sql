a9740164ac59e486d29bc2d29f178dcf56fe5afc
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-02-07 14:13:23 +0000

    Added first steps for creating the tables to the consultant routing.

---
+++ sql/consultant_setup.sql
@@ -0,0 +1,2 @@
+INSERT INTO TB_SKILL (NAME, DESCRIPTION) 
+VALUES('Enterprise Architecture', 'Designing and structuring complex IT systems to align with business goals and ensure efficient, scalable operations.');
