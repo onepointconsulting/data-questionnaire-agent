02e5a1fec4a9e086b7987f6355d8fbbcf7dac2cb
Murtaza Jafari <65913750+jafarimurtaza@users.noreply.github.com>
2025-02-24 10:58:38 +0200

    Refactored delete, update and insert suggestions

--- data_questionnaire_agent/service/persistence_service_questions_async.py
+++ data_questionnaire_agent/service/persistence_service_questions_async.py
@@ -33,19 +33,20 @@ async def select_outstanding_questions(
     language: str, session_id: str
 ) -> list[QuestionAnswer]:
     sql = """
-SELECT Q.ID, TRIM(Q.QUESTION)
-FROM PUBLIC.TB_QUESTION Q
-WHERE Q.LANGUAGE_ID =
-		(SELECT ID
-			FROM PUBLIC.TB_LANGUAGE LANGUAGE_CODE
-			WHERE LANGUAGE_CODE = %(language)s)
-	AND NOT EXISTS
-		(SELECT S.QUESTION_ID
-			FROM TB_QUESTIONNAIRE_STATUS S
-			WHERE S.SESSION_ID = %(session_id)s
-				AND Q.ID = S.QUESTION_ID)
-ORDER BY Q.PREFERRED_QUESTION_ORDER;
-"""
+        SELECT Q.ID, TRIM(Q.QUESTION)
+        FROM PUBLIC.TB_QUESTION Q
+        WHERE Q.LANGUAGE_ID =
+        (SELECT ID
+        FROM PUBLIC.TB_LANGUAGE LANGUAGE_CODE
+        WHERE LANGUAGE_CODE = %(language)s)
+        AND NOT EXISTS
+        (SELECT S.QUESTION_ID
+        FROM TB_QUESTIONNAIRE_STATUS S
+        WHERE S.SESSION_ID = %(session_id)s
+            AND Q.ID = S.QUESTION_ID)
+        ORDER BY Q.PREFERRED_QUESTION_ORDER;
+    """
+
     res = await select_from(sql, {"language": language, "session_id": session_id})
     if res is None:
         return []
@@ -73,27 +74,8 @@ async def select_question_and_suggestions(
 
 
 async def update_question(id: int, question: str, suggestions: list[dict]) -> int:
-    async def process_update(cur: AsyncCursor):
-        updated_question_count = 0
-        await cur.execute(
-            """
-            UPDATE TB_QUESTION
-            SET QUESTION = %(question)s
-            WHERE ID = %(id)s
-            """,
-            {"id": id, "question": question},
-        )
-
-        updated_question_count = cur.rowcount
-
-        insert_suggestion_sql = """
-        INSERT INTO PUBLIC.TB_QUESTION_SUGGESTIONS (ID, IMG_SRC, IMG_ALT, TITLE, MAIN_TEXT, QUESTION_ID, SVG_IMAGE)
-        SELECT (SELECT NEXTVAL('public.tb_question_suggestions_id_seq')), 
-        %(img_src)s, %(img_alt)s, %(title)s, %(main_text)s, %(question_id)s, %(svg_image)s
-        WHERE NOT EXISTS (SELECT ID FROM TB_QUESTION_SUGGESTIONS WHERE ID = %(id)s);
-        """
-
-     # First get al the question suggestion IDs.
+    async def delete_suggestions_process(cur: AsyncCursor, id: int):
+        # First get al the question suggestion IDs.
         get_existing_suggestion_ids_sql = """
         SELECT S.id FROM public.tb_question_suggestions S
         INNER JOIN public.tb_question Q ON Q.id = S.question_id
@@ -102,24 +84,28 @@ async def update_question(id: int, question: str, suggestions: list[dict]) -> in
 
         await cur.execute(get_existing_suggestion_ids_sql, {"id": id})
         rows = await cur.fetchall()
-        print("rows: ", rows)
 
         # And then we need to loop through the suggestions and extract the IDs
         existing_suggestion_ids = {row[0] for row in rows}
-        print(f"Existing suggestion IDs: {existing_suggestion_ids}")
 
         incoming_suggestion_ids = {suggestion["id"]
                                    for suggestion in suggestions}
-        print("incoming_suggestion_ids: ", incoming_suggestion_ids)
 
         ids_to_delete = existing_suggestion_ids - incoming_suggestion_ids
-        print(f"IDs to delete: {ids_to_delete}")
 
         if ids_to_delete:
             await cur.execute(
                 "DELETE FROM public.tb_question_suggestions WHERE id = ANY(%(ids)s)",
                 {"ids": list(ids_to_delete)}
             )
+
+    async def insert_and_update_suggestions_process(cur: AsyncCursor, suggestions: list[dict], id: int):
+        insert_suggestion_sql = """
+        INSERT INTO PUBLIC.TB_QUESTION_SUGGESTIONS (ID, IMG_SRC, IMG_ALT, TITLE, MAIN_TEXT, QUESTION_ID, SVG_IMAGE)
+        SELECT (SELECT NEXTVAL('public.tb_question_suggestions_id_seq')), 
+        %(img_src)s, %(img_alt)s, %(title)s, %(main_text)s, %(question_id)s, %(svg_image)s
+        WHERE NOT EXISTS (SELECT ID FROM TB_QUESTION_SUGGESTIONS WHERE ID = %(id)s);
+        """
 
         update_suggestion_sql = """
         UPDATE PUBLIC.TB_QUESTION_SUGGESTIONS
@@ -132,6 +118,7 @@ async def update_question(id: int, question: str, suggestions: list[dict]) -> in
         SVG_IMAGE = %(svg_image)s
         WHERE ID = %(id)s;
         """
+
         if len(suggestions) > 0:
             for suggestion in suggestions:
                 question_suggestion = QuestionSuggestion(**suggestion)
@@ -160,6 +147,22 @@ async def update_question(id: int, question: str, suggestions: list[dict]) -> in
                             "id": question_suggestion.id,
                         },
                     )
+
+    async def process_update(cur: AsyncCursor):
+        updated_question_count = 0
+        await cur.execute(
+            """
+            UPDATE TB_QUESTION
+            SET QUESTION = %(question)s
+            WHERE ID = %(id)s
+            """,
+            {"id": id, "question": question},
+        )
+
+        updated_question_count = cur.rowcount
+
+        await delete_suggestions_process(cur, id)
+        await insert_and_update_suggestions_process(cur, suggestions, id)
         return updated_question_count
 
     return await create_cursor(process_update, True)
