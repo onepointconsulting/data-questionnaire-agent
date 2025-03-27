162f32d76f68ebb9dd254759bf3b29952aa73410
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-03-27 09:43:45 +0000

    changes to shell scripts

--- deploy_ui.sh
+++ deploy_ui.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
-
-# Change the root folder as needed.
-# The purpose of this script is to deploy the ui and configure it automatically.
-
 cd ~/projects/data-wellness-companion-staging/ui
 rm -rf *
 unzip /home/ubuntu/companion_ui.zip
-sed -i -e 's/8085/8083/g' ./index.html
-sed -i -e 's/127\.0\.0\.1/176.34.128.143/g' ./index.html
+sed -i -e 's/8085/443/g' ./index.html
+sed -i -e 's/127\.0\.0\.1/staging-d-well.onepointltd.ai/g' ./index.html
+sed -i -e 's/ws:/wss:/g' ./index.html
+sed -i -e 's/http:/https:/g' ./index.html
--- run_app.sh
+++ run_app.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-# Change the environment as needed.
+
 ENV_NAME=data_wellness_agent_staging
 source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
 conda activate $ENV_NAME
 cd /home/ubuntu/projects/data-wellness-companion-staging
 
 python ./data_questionnaire_agent/server/questionnaire_server_main.py
+
