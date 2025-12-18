abd51fbb2ba9fd6061bede3e8ec8cca0e39d5201
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-12-18 12:59:20 +0000

    Improved run_app script

--- run_app.sh
+++ run_app.sh
@@ -2,7 +2,7 @@
 ENV_NAME=data_wellness_agent_staging
 source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
 conda activate $ENV_NAME
-cd /home/ubuntu/projects/data-wellness-companion-staging
+cd "$(dirname "$0")"
 
 python ./data_questionnaire_agent/server/questionnaire_server_main.py
 
