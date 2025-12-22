2c77f0a80bb54de7c60b602bbc3454a3cecff080
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-12-22 13:59:39 +0000

    Added database import / export scripts

---
+++ data/dump_db.sh
@@ -0,0 +1,2 @@
+pg_dump -U postgres -h 127.0.0.1 -p 5432 data_wellness_companion_hypergility > expert_matcher.sql
+pg_dump -U postgres -h 127.0.0.1 -p 5432 data_wellness_companion > data_wellness_companion.sql
