4536f8b3f42c3917b0a7c3f402d92eff47b5d665
Gil Fernandes <gil.fernandes@onepointltd.com>
2026-01-22 15:24:03 +0000

    Tried to fix the deployment script

--- deploy_ui.sh
+++ deploy_ui.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-cd ~/projects/data-wellness-companion-hypergility/data-wellness-companion-ui
+cd "$(dirname "$0")"
+
+cd data-wellness-companion-ui
 yarn
 yarn run build
 
-cd ~/projects/data-wellness-companion-hypergility/ui
-rm -rf *
+cd ..
 
-cp -R ~/projects/data-wellness-companion-hypergility/data-wellness-companion-ui/dist/* ~/projects/data-wellness-companion-hypergility/ui
+rm -rf ui/*
+
+cp -R data-wellness-companion-ui/dist/* ui
+
+cd ui
 
 # unzip /home/ubuntu/companion_ui.zip
 sed -i -e 's/8085/443/g' ./index.html
-sed -i -e 's/127\.0\.0\.1/responsible-ai.onepointltd.ai/g' ./index.html
+sed -i -e 's/127\.0\.0\.1/d-well.onepointltd.ai/g' ./index.html
 sed -i -e 's/ws:/wss:/g' ./index.html
 sed -i -e 's/http:/https:/g' ./index.html
